a92e474a00d430c5bac11a8de8cf826880f2a4ac
Aleksei <lexxbar@mail.ru>
2022-08-05 17:56:25 +0200

    Yes

---
+++ Zadacha0011/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Kub(int[] n)
+{
+  int c = 0;
+  int length = n.Length;
+  while (c <  length)
+  {
+    n[c] = Convert.ToInt32(Math.Pow(c, 3));
+    c++;
+  }
+}
+
+void Array(int[] m)
+{
+  int cunt = m.Length;
+  int index = 1;
+  while(index < cunt)
+  {
+    Console.Write(m[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int [n+1];
+Kub(arry);
+Array(arry);
