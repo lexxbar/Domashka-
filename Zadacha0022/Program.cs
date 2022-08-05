746d545b579262808a869a41bf09b913ff9c2d29
Aleksei <lexxbar@mail.ru>
2022-08-05 18:08:42 +0200

    Yes1

---
+++ Zadacha0022/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+	{
+    Console.WriteLine($"Number: {number} - palindrom.");
+  	}
+  else Console.WriteLine($"Number: {number} - Not palindrom .");
+}
+
+if (number!.Length == 5)
+	{
+ 	 CheckingNumber(number);
+	}
+else Console.WriteLine($"Enter another number");
